b5a8f431fa9a3aefa085e038b335885973a3b982
LGJ420 <leegj420@gmail.com>
2024-09-30 22:19:51 +0900

    20240930 수정

--- src/main/java/com/example/gigastudy/controller/CardController.java
+++ src/main/java/com/example/gigastudy/controller/CardController.java
@@ -1,8 +1,16 @@
 package com.example.gigastudy.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
 
 @Controller
 public class CardController {
+    
+    @GetMapping("card")
+    public String getCard() {
+        
+        return "card/card";
+    }
     
 }
