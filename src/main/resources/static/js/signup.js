d0d0588559b8753e5f75252870b0f66dc8932b4f
LGJ420 <leegj420@gmail.com>
2024-10-15 20:20:31 +0900

    회원가입 기능 임시 중지

--- src/main/resources/static/js/signup.js
+++ src/main/resources/static/js/signup.js
@@ -11,19 +11,21 @@ function signup() {
         role: 'USER'
     };
 
-    try {
-        fetch('/api/user', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(data)
-        });
+    alert("현재는 회원가입을 할 수 없습니다. 운영자에게 문의 바랍니다.");
 
-        window.location.href = '/complete';
-    }
-    catch {
-        console.error('회원가입에 실패하였습니다.', error);
-        document.getElementById("errorMessage").style.display = 'block';
-    }
+    // try {
+    //     fetch('/api/user', {
+    //         method: 'POST',
+    //         headers: {
+    //             'Content-Type': 'application/json'
+    //         },
+    //         body: JSON.stringify(data)
+    //     });
+
+    //     window.location.href = '/complete';
+    // }
+    // catch {
+    //     console.error('회원가입에 실패하였습니다.', error);
+    //     document.getElementById("errorMessage").style.display = 'block';
+    // }
 }
