3788415ef573dc8dfa4b844e62fdad0a4104986d
LGJ420 <leegj420@gmail.com>
2024-11-10 03:12:36 +0900

    네비바 대격변

--- src/main/resources/static/js/nav.js
+++ src/main/resources/static/js/nav.js
@@ -1,17 +1,29 @@
+const mobileMenu = document.getElementById('mobileMenu');
+
+menuButton.addEventListener('click', () => {
+    mobileMenu.classList.remove('translate-x-full');
+});
+
+closeButton.addEventListener('click', () => {
+    mobileMenu.classList.add('translate-x-full');
+});
+
+
 document.addEventListener("DOMContentLoaded", function () {
-
     let accessToken = localStorage.getItem('accessToken');
+    let authButtons = document.querySelectorAll(".authButtons");
 
-    if (accessToken) {
-        // JWT가 존재하면 로그아웃 버튼을 표시
-        document.getElementById("authButtons").innerHTML =
-            '<div class="hover:opacity-30 cursor-pointer" onclick="logout()" class="hover:opacity-30">로그아웃</div>';
-    } else {
-        // JWT가 없으면 로그인 버튼을 표시
-        document.getElementById("authButtons").innerHTML =
-            '<a href="/login" class="hover:opacity-30">로그인</a>';
-    }
-    
+    authButtons.forEach(function (element) {
+        if (accessToken) {
+            // JWT가 존재하면 로그아웃 버튼을 표시
+            element.innerHTML =
+                '<div class="hover:opacity-30 cursor-pointer" onclick="logout()">로그아웃</div>';
+        } else {
+            // JWT가 없으면 로그인 버튼을 표시
+            element.innerHTML =
+                '<a href="/login" class="hover:opacity-30">로그인</a>';
+        }
+    });
 });
 
 function logout() {
