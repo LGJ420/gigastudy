9f36fe5a4fd10c54be98bb5d5bdad727b99c410a
LGJ420 <leegj420@gmail.com>
2024-11-09 22:54:06 +0900

    타입 추가

--- src/main/java/com/example/gigastudy/entity/WordType.java
+++ src/main/java/com/example/gigastudy/entity/WordType.java
@@ -4,8 +4,10 @@ public enum WordType {
     
     KANJI,
     JAPANESE,
+    JAPANESE_PARAGRAPH,
     ENGLISH,
     ENGLISH_ORIGIN,
     ENGLISH_EASY1,
     ENGLISH_EASY2,
+    ENGLISH_PARAGRAPH,
 }
