e2116f4ca4dac74c806084c4bb7c496d3259292b
LGJ420 <leegj420@gmail.com>
2024-11-18 01:07:31 +0900

    일본어 테스트버전 글자배치

--- src/main/resources/static/js/wordSimplePass.js
+++ src/main/resources/static/js/wordSimplePass.js
@@ -1,8 +1,10 @@
 let getFlag, getType;
 let currentIndex = 0;
 let words = [];
-let turning = false;
+let extending = false;
 let isAnimating = false;
+let currentCard = 'card1';
+let nextCard = 'card2';
 
 // 사이트 초기화
 async function init(flag, type) {
@@ -18,7 +20,7 @@ async function init(flag, type) {
             getFlag = flag
         }
 
-        // 암기장일때는 삭제버튼 활성화
+        // 암기장일 때 삭제 버튼 활성화
         const deleteButton = document.getElementById('deleteButton');
 
         if (flag === true) {
@@ -26,9 +28,6 @@ async function init(flag, type) {
         }
 
         updateLoadingBar(30);
-
-
-
 
         // JWT 토큰 가져오기
         const accessToken = localStorage.getItem('accessToken');
@@ -41,11 +40,7 @@ async function init(flag, type) {
             }
         });
         currentIndex = await indexResponse.json();
-        updateLoadingBar(70); // 첫번째 요청 완료 후 50%로 설정
-
-
-
-
+        updateLoadingBar(70); // 첫 번째 요청 완료 후 70%로 설정
 
         // 단어 리스트 불러오기
         let wordsUrl = `/api/word?type=${type}`;
@@ -61,12 +56,7 @@ async function init(flag, type) {
             }
         });
         words = await response.json();
-        updateLoadingBar(90); // 두번째 요청 완료 후 90%로 설정
-
-
-
-
-
+        updateLoadingBar(90); // 두 번째 요청 완료 후 90%로 설정
 
         displayWord();
         updateLoadingBar(100); // 완료 후 100%로 설정
@@ -80,13 +70,10 @@ async function init(flag, type) {
         console.error('데이터를 불러오는데 실패하였습니다.', error);
     }
 
-
-
-
     // 단축키 설정
     document.addEventListener('keydown', function (event) {
 
-        // 애니메이션 중이면 단축키 동작 안함
+        // 애니메이션 중이면 단축키 동작 안 함
         if (isAnimating) return;
 
         if (event.code === 'ArrowRight' || event.code === 'Enter' || event.code === 'KeyD') {
@@ -104,9 +91,6 @@ async function init(flag, type) {
     });
 }
 
-
-
-
 // 로딩 화면 표시
 function showLoadingScreen(message) {
     const loadingBarContainer = document.getElementById("loadingBarContainer");
@@ -120,14 +104,11 @@ function showLoadingScreen(message) {
     updateLoadingBar(0);
 }
 
-
-
-
 // 로딩 화면 페이드아웃
 function fadeOutLoadingScreen() {
     const loadingBarContainer = document.getElementById("loadingBarContainer");
 
-    // 페이드아웃 애니메이션이 진행됨
+    // 페이드아웃 애니메이션 진행
     loadingBarContainer.style.opacity = '0';
 
     // 페이드아웃 애니메이션이 끝난 후 로딩바를 제거
@@ -136,23 +117,17 @@ function fadeOutLoadingScreen() {
     }, 500);
 }
 
-
-
-
 // 로딩바 업데이트
 function updateLoadingBar(percent) {
     const loadingBar = document.getElementById("loadingBar");
     loadingBar.style.width = percent + "%";
 }
 
-
-
-
 // 인덱스 저장
 async function saveCurrentIndex() {
 
-    // 단어의 id값을 인덱스로 사용한다.
-    const wordId = words[currentIndex].wordDTO.id;
+    // 단어의 id 값을 인덱스로 사용
+    const wordId = words[currentIndex] ? words[currentIndex].wordDTO.id : 0;
 
     try {
         // JWT 토큰 가져오기
@@ -176,148 +151,176 @@ async function saveCurrentIndex() {
     }
 }
 
-
-
-
 // 단어카드 보여주기
 function displayWord() {
 
-    // 가렸었던 카드 보이기
-    const card = document.getElementById("wordCard");
-    card.classList.remove("opacity-0");
+    // 현재 카드 요소 가져오기
+    const card = document.getElementById(currentCard);
+    const cardFrontFirst = document.getElementById(`${currentCard}FrontFirst`);
+    const cardFrontSecond = document.getElementById(`${currentCard}FrontSecond`);
+    const cardFrontThird = document.getElementById(`${currentCard}FrontThird`);
 
-    const wordDiv = document.getElementById("wordDisplay");
-    const meaningDiv = document.getElementById("wordMeaning");
+    // 다음 카드 숨기기
+    const nextCardElement = document.getElementById(nextCard);
+    nextCardElement.classList.add('hidden');
+    nextCardElement.classList.add('opacity-0');
+    nextCardElement.style.zIndex = '1';
+
+    // 현재 카드 보이기
+    card.classList.remove('hidden');
+    card.classList.remove('opacity-0');
+    card.style.zIndex = '2';
 
     // JWT 토큰 가져오기
     const accessToken = localStorage.getItem('accessToken');
 
     // 로그인 상태 확인
     if (!accessToken) {
-        wordDiv.style.fontSize = '2rem';
-        meaningDiv.style.fontSize = '2rem';
-        
-        wordDiv.textContent = "로그인이 필요한 서비스입니다.";
-        meaningDiv.textContent = "로그인이 필요한 서비스입니다.";
-
+        cardFrontSecond.style.fontSize = '2rem';
+        cardFrontSecond.textContent = "로그인이 필요한 서비스입니다.";
         return;
     }
 
-    // 화면에 카운트 표시
+    // 카드 카운팅 업데이트
     cardCounting();
 
     if (currentIndex < words.length && currentIndex >= 0) {
-        const word = words[currentIndex].wordDTO.word;
+        const pronounce = words[currentIndex].wordDTO.pronounce;
+        const word = cleanWord(words[currentIndex].wordDTO.word);
         const meaning = words[currentIndex].wordDTO.mean1;
+        
 
-        // 글자 길이에 따라 폰트 크기를 조정
-        if (word.length > 10) {
-            wordDiv.style.fontSize = '2rem';
-        } else if (word.length > 6) {
-            wordDiv.style.fontSize = '3rem';
-        } else if (word.length > 4) {
-            wordDiv.style.fontSize = '4rem';
-        } else if (word.length > 2) {
-            wordDiv.style.fontSize = '5rem';
+        // 폰트 크기 조정
+        if (pronounce.length > 3) {
+            cardFrontFirst.style.fontSize = '2rem';
         } else {
-            wordDiv.style.fontSize = '10rem';
+            cardFrontFirst.style.fontSize = '2.5rem';
         }
 
-        if (meaning.length > 10) {
-            meaningDiv.style.fontSize = '2rem';
-        } else if (meaning.length > 6) {
-            meaningDiv.style.fontSize = '3rem';
-        } else if (meaning.length > 4) {
-            meaningDiv.style.fontSize = '4rem';
+        if (word.length > 6) {
+            cardFrontSecond.style.fontSize = '3rem';
+        } else if (word.length > 3){
+            cardFrontSecond.style.fontSize = '4rem';
         } else {
-            meaningDiv.style.fontSize = '5rem';
+            cardFrontSecond.style.fontSize = '5rem';
         }
 
-        wordDiv.textContent = word;
-        meaningDiv.textContent = meaning;
+        if (meaning.length > 25) {
+            cardFrontThird.style.fontSize = '1rem';
+        } else if (meaning.length > 15){
+            cardFrontThird.style.fontSize = '1.3rem';
+        } else if (meaning.length > 10){
+            cardFrontThird.style.fontSize = '1.6rem';
+        } else {
+            cardFrontThird.style.fontSize = '2rem';
+        }
+
+        cardFrontFirst.textContent = pronounce;
+        cardFrontSecond.textContent = word;
+        cardFrontThird.textContent = meaning;
 
     } else {
-        // 단어가 없을 때 폰트 크기를 따로 설정
-        wordDiv.style.fontSize = '2rem';
-        meaningDiv.style.fontSize = '2rem';
-        
-        wordDiv.textContent = "더 이상 단어가 없습니다.";
-        meaningDiv.textContent = "더 이상 단어가 없습니다.";
+        // 더 이상 단어가 없을경우
+        cardFrontSecond.style.fontSize = '2rem';
+        cardFrontSecond.textContent = "더 이상 단어가 없습니다.";
     }
 }
 
-
-
-
-// 다음 단어로 가기
+// 다음 단어로 이동
 function handleClickNext() {
 
-    const card = document.getElementById("wordCard");
-    
+    if (isAnimating) return;
+
+    const card = document.getElementById(currentCard);
+    const cardFrontFirst = document.getElementById(`${currentCard}FrontFirst`);
+    const cardFrontSecond = document.getElementById(`${currentCard}FrontSecond`);
+    const cardFrontThird = document.getElementById(`${currentCard}FrontThird`);
+
     if (currentIndex < words.length) {
-        if (turning) {
+        if (extending) {
             isAnimating = true;
-            const wordDiv = document.getElementById("wordDisplay");
-            const meaningDiv = document.getElementById("wordMeaning");
-            wordDiv.textContent = " ";
-            meaningDiv.textContent = " ";
-            card.classList.add("fly-off");
+
+            // 카드 날아가는 애니메이션 추가
+            card.classList.add('fly-off');
+
+            // 애니메이션동안 다음 카드의 내용 잠시 비우기
+            cardFrontFirst.textContent = " ";
+            cardFrontSecond.textContent = " ";
+            cardFrontThird.textContent = " ";
+
             card.addEventListener('animationend', () => {
-                currentIndex++;
-                turning = false;
-                card.classList.add("opacity-0");
-                card.classList.remove("fly-off");
-                card.classList.remove("flipped");
-                saveCurrentIndex();
-                
+
+                // 애니메이션 종료 후 초기화
+                card.classList.add('hidden');
+                card.classList.add('opacity-0');
+                card.classList.remove('fly-off');
+
                 setTimeout(()=>{
+                    // 다음 카드 준비
+                    currentIndex++;
+                    saveCurrentIndex();
+                    
+                    // currentCard와 nextCard 교체
+                    [currentCard, nextCard] = [nextCard, currentCard];
+
                     displayWord();
                     isAnimating = false;
+                    extending = false;
                 }, 200);
+
             }, { once: true });
+
         } else {
+            // 카드 뒤집기
             isAnimating = true;
-            card.classList.add("flipped");
-            turning = true;
+            extending = true;
             isAnimating = false;
         }
     }
 }
 
-
-
-
-// 이전 단어로 가기
+// 이전 단어로 이동
 function handleClickPre() {
 
-    const card = document.getElementById("wordCard");
+    if (isAnimating) return;
 
-    if (currentIndex === 0 && turning) {
+    const card = document.getElementById(currentCard);
+
+    if (currentIndex === 0 && extending) {
+        // 첫 번째 카드에서 뒤집혀 있을 경우, 뒤집기 취소
         isAnimating = true;
-        card.classList.remove("flipped");
-        turning = false;
+        card.classList.remove('flipped');
+        extending = false;
         isAnimating = false;
+        return;
     }
 
     if (currentIndex > 0) {
-        if (turning) {
+        if (extending) {
+            // 카드 뒤집기 취소
             isAnimating = true;
-            card.classList.remove("flipped");
-            turning = false;
+            card.classList.remove('flipped');
+            extending = false;
             isAnimating = false;
         } else {
+            // 이전 단어로 이동
             currentIndex--;
-            turning = false;
-            card.classList.remove("flipped");
-            card.classList.remove("fly-off");
+            extending = false;
+            isAnimating = true;
+
+            // currentCard와 nextCard 교체
+            [currentCard, nextCard] = [nextCard, currentCard];
+
+            // 이전 카드 준비
             displayWord();
+
+            isAnimating = false;
+
+            // 현재 인덱스 저장
             saveCurrentIndex();
         }
     }
 }
-
-
-
 
 // 암기장에 단어 저장하기
 async function handleClickSave() {
@@ -340,9 +343,6 @@ async function handleClickSave() {
     }
 }
 
-
-
-
 // 암기장에서 단어 삭제하기
 async function handleClickDelete() {
     if (currentIndex < words.length) {
@@ -364,9 +364,6 @@ async function handleClickDelete() {
     }
 }
 
-
-
-
 // 단어 섞기
 async function handleClickShuffle() {
     try {
@@ -377,14 +374,13 @@ async function handleClickShuffle() {
         // JWT 토큰 가져오기
         const accessToken = localStorage.getItem('accessToken');
 
-
         let wordsUrl = `/api/word/shuffle?type=${getType}`;
 
         if (getFlag !== null) {
             wordsUrl += `&flag=${getFlag}`;
         }
 
-        // 단어 섞기 요청 보내기
+        // 단어 섞기 요청
         await fetch(wordsUrl, {
             method: 'POST',
             headers: {
@@ -408,31 +404,29 @@ async function handleClickShuffle() {
     }
 }
 
-
-
-
 // 처음으로 이동
 function handleClickFirst(){
-
     currentIndex = 0;
     displayWord();
     saveCurrentIndex();
 }
 
-
-
-
 // 카드 카운팅
 function cardCounting(){
-
     const countDiv = document.getElementById("cardCount");
 
-    if (currentIndex === words.length) {
-    
-        countDiv.textContent = "finish"
+    if (currentIndex >= words.length) {
+        countDiv.textContent = "finish";
+    } else {
+        countDiv.textContent = `${currentIndex + 1} / ${words.length}`;
     }
-    else {
+}
 
-        countDiv.textContent = `${currentIndex + 1} / ${words.length}`
-    }
+
+// 문자 포맷
+function cleanWord(input) {
+
+    return input
+        .replace(/\|[^|]*\|/g, '') // `|` 로 둘러쌓인 단어 제거
+        .replace(/[·()]/g, '');   // `·`, `(`, `)` 제거
 }
